9f6d9117d2d659add6e200f8b2224f16a65916d8
tim walker <tim.twalker@gmail.com>
2013-03-21 20:34:13 -0700

    unbreak

--- public/js/cilite.js
+++ public/js/cilite.js
@@ -1,9 +1,10 @@
 $(function(){
 	$('nav a').on('click', function(e){
-		
+
 		var $target = $(e.currentTarget);
-		console.log('click', $target.text())
-		$(document.body).removeClass('red yellow green')
-		$(document.body).addClass($target.text());
+
+		$(document.body)
+			.removeClass('red yellow green')
+			.addClass($target.text());
 	});
 });
